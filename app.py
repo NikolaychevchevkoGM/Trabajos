04cb3c02b5ea616021df24e814cbf273ee65ebe0
NicolaychevchevkoGM <nguapacha92@misena.edu.co>
2022-11-29 22:30:46 -0500

    sin comentarios

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import  Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from conex import conexi
 
 app= Flask(__name__)
@@ -60,7 +60,6 @@ def borrar(id):
         return redirect(url_for('index'))
     
 
-    
 
 if __name__ == "__main__":
     app.run(debug=True)
